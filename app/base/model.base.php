2f1eeddc5a73607b870bd186e72b40c507bccd0e
jeffdeleonbsk <jeffdeleonbsk@gmail.com>
2016-01-26 19:36:34 +0800

    use meta for duplicate checking

--- app/base/model.base.php
+++ app/base/model.base.php
@@ -85,18 +85,49 @@ class Model {
     }
     //crud-----------
     public function add(){
+
+        //add mechanism for checking meta
         $request = new Request();
+        $recv_fields = json_decode(json_encode($request->post),true);
+        $recv_fields = array_splice($recv_fields, 1);
+
         // print_r($request);
-        if($request->server->request_method == "POST" && count(json_decode(json_encode($request->post),true)) < 1){
+        if($request->server->request_method == "POST" && count($recv_fields) < 1){
             Response::error_response("No fields recieved on add request");
         } else if($request->server->request_method == "POST"){
-            if(($id = $this->database->insert($this->model_name, json_decode(json_encode($request->post),true))->insert_id)){
-                Response::json_response([
-                        "status_msg" => ucwords(rtrim($this->model_name, "s")) . " added!",
-                        rtrim($this->model_name, "s") . "_id" => $id
-                    ]);
+            //check for duplicates
+            if(count($recv_fields) >= count($this->meta->required_fields)){
+                //check for duplicates\
+                $no_duplicates = false;
+                foreach($this->data as $row){
+                    $user_id = $row[rtrim($this->model_name,"s") . '_id'];
+                    unset($row[rtrim($this->model_name,"s") . '_id']);
+                    if($row == $recv_fields){
+                        $row['user_id'] = $user_id;
+                        Response::json_response([
+                                "status" => 1,
+                                "status_msg" => "User already existed",
+                                "user" => $row
+                            ]);
+                        exit();
+                    } else {
+                        $no_duplicates = true;
+                    }
+                }
+                if($no_duplicates){
+                    $id = $this->database->insert($this->model_name, json_decode(json_encode($request->post),true))->insert_id;
+                    if($id){
+                        Response::json_response([
+                                "status_msg" => ucwords(rtrim($this->model_name, "s")) . " added!",
+                                rtrim($this->model_name, "s") . "_id" => $id
+                            ]);
+                    } else {
+                        Response::error_response("Failed to add " . ucwords(rtrim($this->model_name,"s")));
+                    }
+                }
             } else {
-                Response::error_response("Failed to add " . ucwords(rtrim($this->model_name,"s")));
+                $fields = array_diff($this->meta->required_fields, array_keys($recv_fields));
+                Response::error_response("Total number required fields are not met. The required fields that aren't supplied are " . implode(", ", $fields));
             }
         } else {
             Response::error_response("Wrong method/data type for request.");
